e2a4e003f6a062c9a7426060decd6b7c6694ddea
Abigail deMontrichard <abbiedemont@gmail.com>
2016-12-17 21:00:08 +0000

    Adding hackathon project files

---
+++ index.js
@@ -0,0 +1,24 @@
+/*global $*/
+$("#angry").click(function(){
+    $(".gif").hide()
+ $(".angry").show(); 
+});
+    
+
+ $("#smiling").click(function(){
+     $(".gif").hide()
+     $(".happy").show()
+    });
+
+$("#sad").click(function() {
+    $(".gif").hide()
+    $(".sad").show();
+});
+    $("#love").click(function() {
+        $(".gif").hide() 
+        $(".love").show();
+    
+});
+
+
+
